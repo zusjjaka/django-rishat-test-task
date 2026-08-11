3971cb5f864a58f9a59afc5f60170fa3d08face7
Bogdan <zusjjaka@proton.me>
2026-08-11 15:19:39 +0500

    change csrf trust settings

--- settings/base.py
+++ settings/base.py
@@ -22,7 +22,7 @@ ALLOWED_HOSTS = (
 
 CSRF_TRUSTED_ORIGINS = (
     'http://127.0.0.1:8080',
-    'django-rishat-test-task-production.up.railway.app',
+    'https://django-rishat-test-task-production.up.railway.app',
 )
 
 DJANGO_APPS = (
